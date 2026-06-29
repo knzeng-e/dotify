8981dfe01f2845c9dcd939975c2228acf00294e0
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2026-06-29 22:02:38 +0200

    fix: format

--- web/src/views/PlayerView.tsx
+++ web/src/views/PlayerView.tsx
@@ -179,7 +179,17 @@ export function PlayerView({
           <span
             data-testid='room-playback-mode'
             data-mode={roomPlaybackMode}
-            style={{ position: 'absolute', width: 1, height: 1, padding: 0, margin: -1, overflow: 'hidden', clip: 'rect(0,0,0,0)', whiteSpace: 'nowrap', border: 0 }}
+            style={{
+              position: 'absolute',
+              width: 1,
+              height: 1,
+              padding: 0,
+              margin: -1,
+              overflow: 'hidden',
+              clip: 'rect(0,0,0,0)',
+              whiteSpace: 'nowrap',
+              border: 0
+            }}
           >
             {roomPlaybackMode}
           </span>
